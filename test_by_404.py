2ee36672e147010c6e14a1076a5ecd6f36caee54
HellFire404 <86830928+HellFire404@users.noreply.github.com>
2021-07-02 18:31:13 +0800

    Update test_by_404.py

--- test_by_404.py
+++ test_by_404.py
@@ -1,3 +1,4 @@
 import numpy
 print("so,this is the python created by 404!")
 print("this mean that i can create my own files in the other people's project in github.")
+print("this is the test for collaborator to verify operation")
